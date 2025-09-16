801b7a0f1e4c6a010f5c350627fabc4760c79cf0
Ponzu-IKA <138089174+Ponzu-IKA@users.noreply.github.com>
2025-09-17 02:32:59 +0900

    update: hyprland package

--- home/configurations/wm/hyprland/default.nix
+++ home/configurations/wm/hyprland/default.nix
@@ -6,6 +6,12 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
+
+    package = null;
+    portalPackage = null;
+
     extraConfig = ( builtins.readFile ../../../config/hyprland/hyprland.conf );
+    systemd.variables = ["--all"];
+    systemd.enable = true;
   };
 }
