d0b8db2fca41c420aea5f8cdf697fe152a626eac
Ponzu-IKA <138089174+Ponzu-IKA@users.noreply.github.com>
2025-09-17 22:49:38 +0900

    refactor: 記述ミスを修正

--- home/configurations/cli/vcs/default.nix
+++ home/configurations/cli/vcs/default.nix
@@ -3,19 +3,21 @@
     lazygit = {
       enable = true;
       settings = {
-        customCommands = {
-          key = "E";
-          description = "空コミット";
-          context = "global";
-          command = "git commit --allow-empty -m '{{index .PromptResoinses 0}}'";
-          prompts = [
-            {
-              type = "input";
-              title = "Commit message";
-              initialValue = "chore: empty commit";
-            }
-          ];
-        };
+        customCommands = [
+          {
+            key = "E";
+            description = "空コミット";
+            context = "global";
+            command = "git commit --allow-empty -m '{{index .PromptResoinses 0}}'";
+            prompts = [
+              {
+                type = "input";
+                title = "Commit message";
+                initialValue = "chore: empty commit";
+              }
+            ];
+          }
+        ];
       };
     };
   };
