ecda062a04246c42a6fedb9675af06e00f9d8e2b
Amai-ICE <138089174+Amai-ICE@users.noreply.github.com>
2025-11-26 07:13:51 +0900

    refactor: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,44 +1,50 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # 再現性100%のHOME環境を作るえらいこ.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
       url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs: {
-    homeConfigurations = {
-      amaiice = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs {
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      nixvim,
+      ...
+    }@inputs:
+    {
+      homeConfigurations = {
+        amaiice = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = import inputs.nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+          extraSpecialArgs = {
+            inherit nixvim;
+          };
+          modules = [
+            ./home/configurations
+          ];
+        };
+      };
+      nixosConfigurations = {
+        nixos = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-        extraSpecialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          ./home/configurations
-
-          inputs.nixvim.homeManagerModules.nixvim
-        ];
-      };
-    };
-    nixosConfigurations = {
-      nixos = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./os/configurations
-        ];
-        specialArgs = {
-          inherit inputs;
+          modules = [
+            ./configuration.nix
+            ./os/configurations
+          ];
+          specialArgs = {
+            inherit inputs;
+          };
         };
       };
     };
-  };
 }
