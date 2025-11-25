badcac21430b5e6a551d7f790af11785b2e56110
Amai-ICE <138089174+Amai-ICE@users.noreply.github.com>
2025-11-26 07:13:13 +0900

    update: alacritty

--- home/configurations/gui/alacritty/settings.nix
+++ home/configurations/gui/alacritty/settings.nix
@@ -1,21 +1,24 @@
 {
-  programs.alacritty.settings = {
-    font = {
-      normal = {
-        family = "JetBrainsMono Nerd Font";
-        style = "Regular";
+  programs.alacritty = {
+    theme = "kanagawa_dragon";
+    settings = {
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Italic";
+        };
       };
-      bold = {
-        family = "JetBrainsMono Nerd Font";
-        style = "Bold";
+      window = {
+        opacity = 0.65;
       };
-      italic = {
-        family = "JetBrainsMono Nerd Font";
-        style = "Italic";
-      };
-    };
-    window = {
-      opacity = 0.0;
     };
   };
 }
