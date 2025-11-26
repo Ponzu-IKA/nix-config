be6ad2a8685e4d244b40b33f3dfa88b224ff24d2
Amai-ICE <138089174+Amai-ICE@users.noreply.github.com>
2025-11-26 10:41:54 +0900

    update: entangled.nix

--- home/configurations/gui/entangled.nix
+++ home/configurations/gui/entangled.nix
@@ -1,58 +1,35 @@
 {
+  pkgs,
   lib,
-  fetchFromGitHub,
-  rustPlatform,
-  python3,
-  wayland,
-  xorg,
-  libxkbcommon,
-  libGL,
+  ...
 }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "noita_entangled_worlds";
-  version = "v0.32.6";
+let
+  noitaEW = (
+    pkgs.stdenv.mkDerivation rec {
+      pname = "noita_proxy";
+      version = "v1.6.2";
 
-  src = fetchFromGitHub {
-    owner = "IntQuant";
-    repo = pname;
-    rev = version;
-    hash = "sha256-fpj4vBhMws3o3Gs/hq94+ITd4AtuKoVHnAlt/PWwLIU=";
-  };
+      src = pkgs.fetchzip {
+        url = "https://github.com/IntQuant/noita_entangled_worlds/releases/tag/${version}/noita-proxy-linux.zip ";
+        sha256 = "sha256-0";
+      };
 
-  sourceRoot = "source/noita-proxy";
-  cargoHash = "sha256-AMnI0mvSuEDRPolcBgR6aCXaiRgMn1qpWjlksLjhvwQ=";
+      nativeBuildInputs = with pkgs; [
+        makeWrapper
+        unzip
+      ];
 
-  # tests fail due to no internet
-  doCheck = false;
+      installPhase = ''
+        mkdir -p $out/bin
 
-  nativeBuildInputs = [ python3 ];
-  buildInputs = [
-    libxkbcommon
-    libGL
+        install -m755 ${pname} $out/bin/${pname}-real
 
-    # WINIT_UNIX_BACKEND=wayland
-    wayland
-
-    # WINIT_UNIX_BACKEND=x11
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXi
-    xorg.libX11
-  ];
-
-  postInstall = ''
-    mkdir -p $out/lib
-    cp $src/redist/libsteam_api.so $out/lib/libsteam_api.so
-  '';
-
-  postFixup = ''
-    patchelf --add-rpath "${lib.makeLibraryPath buildInputs}" $out/bin/noita-proxy
-  '';
-  meta = {
-    description = "Noita Co-op multiplayer mod";
-    homepage = "https://github.com/IntQuant/noita_entangled_worlds";
-    license = lib.licenses.mit;
-    maintainers = [ ];
-  };
+        wrapProgram $out/bin/${pname}-real --prefix steam-run
+      '';
+    }
+  );
+in
+{
+  home.packages = with pkgs; [ noitaEW ];
 }
